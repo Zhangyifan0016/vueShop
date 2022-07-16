d4cfa966295db41f5ab18ff2177e9b77f110be48
Zhangyifan0016 <z351072259@163.com>
2022-07-16 11:47:27 +0800

    feat(login api styles): 实现login布局并成功调取接口 封装登录api 在main.js引入公共样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 // 引入ElementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 引入公共样式
+import './styles/reset.scss'
 
 const app = createApp(App)
 
